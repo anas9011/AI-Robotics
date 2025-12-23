16a6a330b2e2cdb9dac9a43855717063ad82ed37
Anas Shakeel <anasshakeel9011@gmail.com>
2025-12-24 03:24:42 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://localhost:3000',
+  url: 'https://vercel.com/anas-shakeels-projects/ai-robotics-gwtt',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ros2-humanoid-robotics-book/',
