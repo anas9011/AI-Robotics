6fde9d77c05c8f6d33855d84a89cf8c83ea9857a
Anas Shakeel <anasshakeel9011@gmail.com>
2025-12-24 18:55:32 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   url: 'https://ai-robotics-gwtt.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://vercel.com/anas-shakeels-projects/ai-robotics-gwtt',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'your-organization', // Usually your GitHub org/user name.
